92e0a0884cb552ab99c365b2d5c322b0f40562a8
sanghoon <digimon.1740@gmail.com>
2022-06-05 21:13:42 +0900

    init

---
+++ src/test/kotlin/com/fastcampus/kopring/userservice/UserserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fastcampus.kopring.userservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
